357020570f26f488c19826b7f58ab474b16cf00c
amarkishan <amar6333kartheek@gmail.com>
2025-03-11 21:21:42 -0400

    this is new

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
           <div className="about-text">
             <h2>About Me</h2>
             <p> Amar Vaka :Full Stack Developer</p>
-            <p>I am a Full Stack Developer with 8+ years of experience in designing and developing scalable, high-performance applications. My expertise spans Java, Spring Boot microservices, and front-end technologies like ReactJS, enabling me to build end-to-end solutions that are efficient, secure, and user-friendly. I have a strong background in cloud technologies (AWS, Azure), CI/CD pipelines, and DevOps tools like Docker and Kubernetes, ensuring seamless deployment and scalability.</p>
+            <p>I am a Full Stack Developer with 8+ years of experience in designing and developing scalable, high-performance applications. My expertise spans <b>Java</b>, Spring Boot microservices, and front-end technologies like <b>ReactJS</b>,<b>Angular</b> enabling me to build end-to-end solutions that are efficient, secure, and user-friendly. I have a strong background in cloud technologies (AWS, Azure), CI/CD pipelines, and DevOps tools like Docker and Kubernetes, ensuring seamless deployment and scalability.</p>
             <p>With a deep understanding of microservices architecture, messaging systems (Kafka, RabbitMQ), and database management (SQL & NoSQL), I have successfully optimized data pipelines, enhanced system reliability, and contributed to enterprise-grade solutions in industries like healthcare, retail, and finance. I am passionate about leveraging modern technologies to solve complex problems and continuously improving application performance and user experience.</p>
             <a href={cv} className="btn">Download Resume</a>
           </div>
